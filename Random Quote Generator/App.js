f907019283f470742cb01e402e48347ce2633ab8
Arnab Mitra <71271613+ar319@users.noreply.github.com>
2022-11-03 07:07:21 -0700

    Add files via upload

---
+++ Random Quote Generator/App.js
@@ -0,0 +1,46 @@
+import './App.css';
+
+import React, { useState, useEffect } from "react";
+
+const App = () => {
+  var [quote, setQuotes] = useState([{}]);
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  const getQuotes = async (e) => {
+    const data = await fetch("https://type.fit/api/quotes");
+    const response = await data.json();
+    setQuotes(response);
+  };
+
+  function randQuote() {
+    let tempQuote = Math.floor(Math.random() * quote.length);
+    return tempQuote;
+  }
+
+  return (
+    <div className="App">
+      <main>
+        <div className="text">"{quote[randQuote()].text}"</div>
+        <div className="author">
+          ~
+          {quote[randQuote()].author != null
+            ? quote[randQuote()].author
+            : "Unknown"}
+        </div>
+        <br />
+        <button
+          className="button"
+          onClick={(e) => {
+            getQuotes();
+          }}
+        >
+          Press here to get a random quote
+        </button>
+      </main>
+    </div>
+  );
+};
+
+export default App;
